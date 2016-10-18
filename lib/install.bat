5f4b48bbcf8494bb496a41e3905b07a87fcc0301
Jim Durand <me@jdurand.com>
2016-10-18 16:41:10 +0100

    Skip configuration if CONFIGURATION file exists

--- lib/install.bat
+++ lib/install.bat
@@ -30,15 +30,19 @@ call gem uninstall rubygems-update -x
 call gem install bundler --no-rdoc --no-ri
 call bundle install --without development test
 
-@echo Configuring Libro Sync service...
-call set /P WORKING_DIR= "File path to watch [files]: "
-call set /P LIBRO_API_TOKEN= "Libro API token: "
-call set /P RESTAURANT_CODE= "Restaurant code: "
-rem IF "%WORKING_DIR%"=="" (SET WORKING_DIR=files)
-call del CONFIGURATION
-@echo WORKING_DIR=%WORKING_DIR%>CONFIGURATION
-@echo LIBRO_API_TOKEN=%LIBRO_API_TOKEN%>>CONFIGURATION
-@echo RESTAURANT_CODE=%RESTAURANT_CODE%>>CONFIGURATION
+if exist CONFIGURATION (
+  @echo Configuration file exists; Skipping configuration...
+) else (
+  @echo Configuring Libro Sync service...
+  call set /P WORKING_DIR= "File path to watch [files]: "
+  call set /P LIBRO_API_TOKEN= "Libro API token: "
+  call set /P RESTAURANT_CODE= "Restaurant code: "
+  rem IF "%WORKING_DIR%"=="" (SET WORKING_DIR=files)
+  rem call del CONFIGURATION
+  @echo WORKING_DIR=%WORKING_DIR%>CONFIGURATION
+  @echo LIBRO_API_TOKEN=%LIBRO_API_TOKEN%>>CONFIGURATION
+  @echo RESTAURANT_CODE=%RESTAURANT_CODE%>>CONFIGURATION
+)
 
 @echo Installing Libro Sync service...
 call ruby lib/register.rb
