4120a1178418fa000d6a6a43bb2c3ccf4e6bb4f3
Jim Durand <me@jdurand.com>
2017-12-06 11:47:01 -0500

    Use https to download git

--- lib/install.bat
+++ lib/install.bat
@@ -27,7 +27,7 @@ IF ERRORLEVEL 1 (
   IF NOT exist vendor\git-2.10.1-32-bit.exe (
     IF NOT exist vendor ( mkdir vendor )
     @echo Downloading Git...
-    powershell -command "$clnt = new-object System.Net.WebClient; $clnt.DownloadFile(\"http://bitbucket.org/jimdurand/libro-sync-agent/downloads/git-2.10.1-32-bit.exe\", \"c:\libro-sync-agent\vendor\git-2.10.1-32-bit.exe\")"
+    powershell -command "$clnt = new-object System.Net.WebClient; $clnt.DownloadFile(\"https://bitbucket.org/jimdurand/libro-sync-agent/downloads/git-2.10.1-32-bit.exe\", \"c:\libro-sync-agent\vendor\git-2.10.1-32-bit.exe\")"
   )
   @echo Installing Git...
   call vendor\git-2.10.1-32-bit /verysilent /tasks="modpath"
